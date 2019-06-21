6273b2d514b67ab93c8fdce7783ae19e0643c08a
hai_van <hvandang@outlook.com>
2019-06-21 17:58:10 +0100

    Retrieve path of log_file from config_file

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,13 @@ from app import openidc
 #if not app.debug:
 # initialize the log handler: The handler used is RotatingFileHandler which rotates the log file when the size of the file exceeds a certain limit.
 #cfg_path = os.path.abspath(os.path.join(os.path.dirname(__file__),"../../"))
-error_path = '/tmp/cfg_error.log'
+cfg_path = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+json_path = os.path.join(cfg_path, 'config.json')
+with open(json_path,'r') as f:
+	config = json.load(f)
+
+error_path = config['DEFAULT']['LOG_FILE']
+# error_path = '/tmp/cfg_error.log'
 
 logHandler = RotatingFileHandler(error_path, maxBytes=1000, backupCount=1) 
 # set the log handler level
