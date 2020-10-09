7de47dea2b1ac725919402670465595244319cfd
hvandang@outlook.com <hvandang@outlook.com>
2020-10-09 15:11:57 +0100

    fix: fix listening port

--- parameters.py
+++ parameters.py
@@ -6,9 +6,9 @@ import logging
 # if os.getenv("KEYCLOAK_SERVER") is None and os.getenv("KEYCOLAK_REALM") is None:
 #     raise Exception("Values for Keycloak server and realm isn't avalable")
 
-keycloak_server = os.getenv("KEYCLOAK_SERVER","http://127.0.0.1:8080/auth/")
+keycloak_server = os.getenv("KEYCLOAK_SERVER","http://kc.cfg.cpc.uow/auth/")
 keycloak_realm = os.getenv("KEYCOLAK_REALM","cfg")
-server_port = os.getenv("API_PORT", "8081")
+server_port = os.getenv("API_PORT", "8080")
 emgum_api_url_context = os.getenv("URL_CONTEXT", "/emgum/api/v1.0/")
 postgres_server = os.getenv("DB_ADDR")
 postgres_username = os.getenv("DB_USER")
